f9771a4c83d7a188c82595a83918c634ed723d1a
pvs3 <35466280+pvs3@users.noreply.github.com>
2023-09-06 17:44:25 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
-let pin0 = 0
 let x = 0
 let y = 0
+let pin0 = 0
+function ledsOn (aantal: number) {
+    for (let index = 0; index <= aantal; index++) {
+        x = index / 5
+        y = index % 5
+        led.plot(x, y)
+    }
+}
 basic.forever(function () {
     serial.writeString("hallo")
     serial.writeLine("")
     pin0 = pins.analogReadPin(AnalogPin.P0)
     serial.writeNumber(pin0)
     serial.writeLine("")
-    while (pin0 >= 700) {
-        serial.writeNumber(pins.analogReadPin(AnalogPin.P0))
-        x = randint(0, 4)
-        y = randint(0, 4)
-        led.toggle(x, y)
-        basic.pause(100)
-        pin0 = pins.analogReadPin(AnalogPin.P0)
-    }
-    basic.pause(100)
+    pin0 = pin0 - 660
+    pin0 = Math.map(pin0, 0, 363, 0, 24)
+    ledsOn(pin0)
+    basic.pause(1000)
     basic.clearScreen()
 })
